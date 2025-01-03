418e060a450fa9013e649ed824e472be02469d3e
carok76 <carolin.krahmer@gmail.com>
2025-01-03 11:02:01 +0100

    Update index.js: deleted console log from testing

--- src/index.js
+++ src/index.js
@@ -44,7 +44,6 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
   let forecastHtml = "";
 
   response.data.daily.forEach(function(day, index) {
